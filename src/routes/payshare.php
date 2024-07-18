163167fda38ebd44b3e7f002c31e12853320ed12
davidle90 <89838376+davidle90@users.noreply.github.com>
2024-07-18 04:35:29 +0200

    update view layout

--- src/routes/payshare.php
+++ src/routes/payshare.php
@@ -3,7 +3,7 @@
 use Davidle90\Payshare\app\Http\Controllers\PayshareController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/payshare', [PayshareController::class, 'index'])->name('payshare.index');
+Route::get('projects/payshare', [PayshareController::class, 'index'])->name('payshare.index');
 
 Route::group(['middleware' => ['web', 'auth']], function () {
 
